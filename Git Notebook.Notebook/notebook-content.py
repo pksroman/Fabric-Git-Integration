c95e84590e33ed4b0180aed627aa71a329dc9c8c
pksroman <65640000+pksroman@users.noreply.github.com>
2026-07-05 15:41:35 +0530

    Created a notebook and added 1 line of code

---
+++ Git Notebook.Notebook/notebook-content.py
@@ -0,0 +1,21 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {}
+# META }
+
+# CELL ********************
+
+print("Welcome to the last day of the training")
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
